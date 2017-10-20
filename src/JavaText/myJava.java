adb07dc0c0765ca241a37972b27c57949e139d4f
xinhw <xhw_180@163.com>
2017-10-20 13:48:30 +0800

    测试

--- src/JavaText/myJava.java
+++ src/JavaText/myJava.java
@@ -5,6 +5,7 @@ public class myJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("myJava.main()");
+		System.out.println("myJava.main()");
 	}
 
 }
